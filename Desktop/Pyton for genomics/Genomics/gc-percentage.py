1638ebafbd3d17b9c7167d3e5b5d7bf8c2aad2c5
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-02-22 21:36:40 +0200

    Counting gc and total valid length

--- Desktop/Pyton for genomics/Genomics/gc-percentage.py
+++ Desktop/Pyton for genomics/Genomics/gc-percentage.py
@@ -1,9 +1,11 @@
 sequence= input("Provide a valid DNA sequence!: ")
 
 sequence = sequence.upper()  # Convert to uppercase
-valid_nucleotides = {'A', 'T', 'G', 'C'}  # Set of valid DNA bases
-print (valid_nucleotides)
-    
+valid_nucleotides = {'A', 'T', 'G', 'C'}  # Set of valid DNA bases  
 filtered_sequence = [base for base in sequence if base in valid_nucleotides]
 print(filtered_sequence)
 
+gc_count = filtered_sequence.count('G') + filtered_sequence.count('C')
+total_valid_bases = len(filtered_sequence)
+print (gc_count)
+print(total_valid_bases)
