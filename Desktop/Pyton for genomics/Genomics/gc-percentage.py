578af6f29e5b753d9596e4defef831d40bda3117
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-02-22 22:22:38 +0200

    Printing readable output

--- Desktop/Pyton for genomics/Genomics/gc-percentage.py
+++ Desktop/Pyton for genomics/Genomics/gc-percentage.py
@@ -3,16 +3,16 @@ sequence= input("Provide a valid DNA sequence!: ")
 sequence = sequence.upper()  # Convert to uppercase
 valid_nucleotides = {'A', 'T', 'G', 'C'}  # Set of valid DNA bases  
 filtered_sequence = [base for base in sequence if base in valid_nucleotides]
-print(filtered_sequence)
+print("The valid sequence I am working on is: ","".join(filtered_sequence))
 
 gc_count = filtered_sequence.count('G') + filtered_sequence.count('C')
 total_valid_bases = len(filtered_sequence)
-print (gc_count)
-print(total_valid_bases)
+print ("The number of 'g' and 'c' bases in the provided DNA sequence is = ",gc_count)
+print("The total valid bases in the provided DNA sequence is = ",total_valid_bases)
 
 if total_valid_bases == 0:
     print (0.0)
 
 gc_percentage = (gc_count/total_valid_bases)*100
-print (round(gc_percentage,3))
+print ("The 'gc' content in this DNA sequence is = ",round(gc_percentage,3), "%")
 
