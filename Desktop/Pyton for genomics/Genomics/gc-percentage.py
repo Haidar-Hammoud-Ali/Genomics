0ba530a7764218fd84e7263286ea981514278195
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-02-26 23:42:55 +0200

    Defining GC % function

--- Desktop/Pyton for genomics/Genomics/gc-percentage.py
+++ Desktop/Pyton for genomics/Genomics/gc-percentage.py
@@ -1,3 +1,4 @@
+
 while True :
     sequence= input("Provide a valid DNA sequence! (Type 'stop' to exit): ")
     if sequence.lower() == "stop":
@@ -18,3 +19,24 @@ while True :
         gc_percentage = (gc_count/total_valid_bases)*100
         print ("The 'GC' % in this DNA sequence is = ",round(gc_percentage,3), "%")
 
+
+def gc_percentage (sequence):
+    sequence = sequence.upper()
+    valid_nucleotides = {'A', 'T', 'G', 'C'}
+    filtered_sequence = [base for base in sequence if base in valid_nucleotides]
+    print("The valid sequence I am working on is: ","".join(filtered_sequence))
+
+    gc_count = filtered_sequence.count('G') + filtered_sequence.count('C')
+    total_valid_bases = len(filtered_sequence)
+    print ("The number of 'g' and 'c' bases in the provided DNA sequence is = ",gc_count)
+    print("The total valid bases in the provided DNA sequence is = ",total_valid_bases)
+
+    if total_valid_bases == 0:
+        print ("The 'GC' % in this DNA sequence is = 0.0%")
+    else:
+        gc_percentage = (gc_count/total_valid_bases)*100
+        print ("The 'GC' % in this DNA sequence is = ",round(gc_percentage,3), "%")
+    return (gc_percentage)
+
+result= gc_percentage ("asadcagfscdfacdascxsacxyascdtascxfasccxasdysacxyacxycadcascdyascxywqcyxcasydcayscxaytxyascdytascxgasycdyasxcysaftyascytasghxcays")
+print (result)
